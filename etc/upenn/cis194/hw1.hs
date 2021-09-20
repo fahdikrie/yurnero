56658e4958de30345adf8d74d37adc1dbb454d09
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-09-20 23:50:37 +0700

    etc: add cis194 folder

---
+++ etc/upenn/cis194/hw1.hs
@@ -0,0 +1,5 @@
+{-# OPTIONS_GHC -Wall #-}
+-- Ref: https://www.seas.upenn.edu/~cis194/spring13/hw/01-intro.pdf
+
+-- Problem 1 — Validating Credit Card Numbers
+validateCreditCardNum :: [Integer] ->
